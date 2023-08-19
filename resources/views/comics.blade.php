60ca77f3fed97470bc790f3d21ec32e53667f839
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-19 21:58:12 +0200

    changed icon path

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,7 +6,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Comics</title>
-    <link rel="icon" href="{{ asset('images/favicon.ico') }}" type="image/png">
+    <link rel="icon" href="{{ Vite::asset('resources/images/favicon.ico') }}" type="image/png">
 
     {{-- importo gli assets --}}
     @vite('resources/js/app.js')
