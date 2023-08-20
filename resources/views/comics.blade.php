43cdb900fc7678a020f8c27870545a7cc3441f19
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-20 12:12:26 +0200

    added header & footer content

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -13,7 +13,74 @@
 </head>
 
 <body>
+    <!-- header -->
+    <header class="container">
+        <figure>
+            <img src="{{ Vite::asset('resources/images/dc-logo.png') }}" alt="logo">
+        </figure>
+        <nav>
+            <ul>
+                <li><a href="#"> Characters </a></li>
+                <li><a href="#"> Comics </a></li>
+                <li><a href="#"> Movies </a></li>
+                <li><a href="#"> Tv </a></li>
+                <li><a href="#"> Games </a></li>
+                <li><a href="#"> Collectibles </a></li>
+                <li><a href="#"> Videos </a></li>
+                <li><a href="#"> Fans </a></li>
+                <li><a href="#"> News </a></li>
+                <li><a href="#"> Shop </a></li>
+            </ul>
+        </nav>
+    </header>
 
+    <!-- content section -->
+
+
+
+    <footer class="bgc-footer">
+        <!-- footer nav -->
+        <section class="container footer-flex ">
+            <nav>
+                <div class="comicsnav">
+                    <h1>DC COMICS</h1>
+                    <ul>
+                        <li><a href="#"> Characters </a></li>
+                        <li><a href="#"> Comics </a></li>
+                        <li><a href="#"> Movies </a></li>
+                        <li><a href="#"> Tv </a></li>
+                        <li><a href="#"> Games </a></li>
+                        <li><a href="#"> Collectibles </a></li>
+                        <li><a href="#"> Videos </a></li>
+                        <li><a href="#"> Fans </a></li>
+                        <li><a href="#"> News </a></li>
+                        <li><a href="#"> Shop </a></li>
+
+                    </ul>
+                    <h1>SHOP</h1>
+                    <ul>
+                        {{--  <li v-for="item in shopItems">{{ item }}</li> --}}
+                    </ul>
+                </div>
+
+                <div class="dcnav">
+                    <h1>DC</h1>
+                    <ul>
+                        {{--  <li v-for="item in dcItems">{{ item }}</li> --}}
+                    </ul>
+                </div>
+                <div class="shopnav">
+                    <h1>SITES</h1>
+                    <ul>
+                        {{-- <li v-for="item in sitesItems">{{ item }}</li> --}}
+                    </ul>
+                </div>
+            </nav>
+
+            <div class="bgc-right"></div>
+        </section>
+
+    </footer>
 </body>
 
 </html>
