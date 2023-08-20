5e637996c144756b1c1c0facf7590ef28cf8823c
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-20 17:33:36 +0200

    fixed footer contents

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -37,7 +37,7 @@
     <!-- content section -->
 
 
-
+    <!-- footer  -->
     <footer class="bgc-footer">
         <!-- footer nav -->
         <section class="container footer-flex ">
@@ -59,27 +59,58 @@
                     </ul>
                     <h1>SHOP</h1>
                     <ul>
-                        {{--  <li v-for="item in shopItems">{{ item }}</li> --}}
+                        <li><a href="#"> Shop DC </a></li>
+                        <li><a href="#"> Shop DC Collectibles </a></li>
                     </ul>
                 </div>
 
                 <div class="dcnav">
                     <h1>DC</h1>
                     <ul>
-                        {{--  <li v-for="item in dcItems">{{ item }}</li> --}}
+                        <li><a href="#"> Term Of Use </a></li>
+                        <li><a href="#"> Privacy policy(New) </a></li>
+                        <li><a href="#"> Ad Choises </a></li>
+                        <li><a href="#"> Advertising </a></li>
+                        <li><a href="#"> Jobs </a></li>
+                        <li><a href="#"> Talent Workshop </a></li>
+                        <li><a href="#"> CPSC Certificates </a></li>
+                        <li><a href="#"> Ratings </a></li>
+                        <li><a href="#"> Shop Help </a></li>
+                        <li><a href="#"> Contact Us </a></li>
+
                     </ul>
                 </div>
                 <div class="shopnav">
                     <h1>SITES</h1>
                     <ul>
-                        {{-- <li v-for="item in sitesItems">{{ item }}</li> --}}
+                        <li><a href="#"> DC </a></li>
+                        <li><a href="#"> MAD Magazine </a></li>
+                        <li><a href="#"> DC Kids </a></li>
+                        <li><a href="#"> DC Universe </a></li>
+                        <li><a href="#"> DC Power Visa </a></li>
+
                     </ul>
                 </div>
             </nav>
 
             <div class="bgc-right"></div>
         </section>
+        <div class="footer-user">
+            <div class="footer-flex container">
+                <div class="signup-btn">
+                    <h3>SIGN UP NOW!</h3>
+                </div>
+                <div class="followus">
+                    <h3> FOLLOW US </h3>
+                    <img src="{{ Vite::asset('resources/images/footer-facebook.png') }}" alt="fb">
+                    <img src="{{ Vite::asset('resources/images/footer-periscope.png') }}" alt="pr">
+                    <img src="{{ Vite::asset('resources/images/footer-pinterest.png') }}" alt="pi">
+                    <img src="{{ Vite::asset('resources/images/footer-twitter.png') }}" alt="tw">
+                    <img src="{{ Vite::asset('resources/images/footer-youtube.png') }}" alt="yt">
 
+                </div>
+            </div>
+        </div>
     </footer>
 </body>
 
