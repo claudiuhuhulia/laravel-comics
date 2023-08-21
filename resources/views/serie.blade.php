901674c6df8f0cb00a3b05d883abfb01558731e6
cloudhuh <claudiu.huhulia@gmail.com>
2023-08-21 17:23:23 +0200

    price btn style

--- resources/views/serie.blade.php
+++ resources/views/serie.blade.php
@@ -12,8 +12,21 @@
         </section>
     </div>
 
-    <div id="serie-detail" class="container">
-
+    <div class="container serie-detail">
+        <div class="text">
+            <h1>{{ $serie['title'] }}</h1>
+            <div class="price-btn">
+                <div class="price">
+                    <h4>U.S. Price: <p>{{ $serie['price'] }}</p>
+                    </h4>
+                    <h4>AVAILABLE</h4>
+                </div>
+                <div class="availability">
+                    <p>Check availability </p>
+                </div>
+            </div>
+        </div>
+        <div class="adv"></div>
 
 
     </div>
